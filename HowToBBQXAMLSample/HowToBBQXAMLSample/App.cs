a45c9491344d1ea08d3782063299111717a6958d
Tom Walker <tom.walker1313@gmail.com>
2015-11-26 10:12:28 -0500

    Updated to Xamarin.Forms 2.0

---
+++ HowToBBQXAMLSample/HowToBBQXAMLSample/App.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HowToBBQXAMLSample.Views;
+
+using Xamarin.Forms;
+
+namespace HowToBBQXAMLSample
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var recipes = new BBQRecipesPage();
+            MainPage = new NavigationPage(recipes);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ HowToBBQXAMLSample/HowToBBQXAMLSample/Views/AspectImageCell.cs
@@ -0,0 +1,68 @@
+﻿using Xamarin.Forms;
+
+namespace HowToBBQXAMLSample.Views
+{
+    public class AspectImageCell : ViewCell
+    {
+
+        public static readonly BindableProperty NameProperty =
+            BindableProperty.Create<AspectImageCell, string>(
+                p => p.Name, string.Empty);
+
+
+        public static readonly BindableProperty ImageSourceProperty =
+        BindableProperty.Create<AspectImageCell, string>(
+        p => p.ImageSource, string.Empty);
+
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set
+            {
+                SetValue(NameProperty, value);
+
+            }
+        }
+
+        public string ImageSource
+        {
+            get { return (string)GetValue(ImageSourceProperty); }
+            set
+            {
+                SetValue(ImageSourceProperty, value);
+
+            }
+        }
+
+        public AspectImageCell()
+        {
+            var label = new Label
+            {
+                YAlign = TextAlignment.Center,
+                Font = Font.SystemFontOfSize(15)
+            };
+
+            label.SetBinding(Label.TextProperty, "Name");
+
+            var foodImage = new Image
+            {
+                WidthRequest = 40,
+                HeightRequest = 60
+            };
+
+            foodImage.SetBinding(Image.SourceProperty, "ImageSource");
+
+
+            var layout = new StackLayout
+            {
+                Padding = new Thickness(0, 0, 10, 0),
+                Orientation = StackOrientation.Horizontal,
+                HorizontalOptions = LayoutOptions.StartAndExpand,
+                Children = { foodImage, label }
+            };
+
+            View = layout;
+        }
+    }
+
+}
